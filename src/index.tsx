4eac5527a96fdc5eb0b42da90a789bb2d482b378
Gavin Atkinson <gvnatknsn@protonmail.com>
2021-10-06 14:02:16 -0400

    layout to mocks

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './App.scss';
 
 ReactDOM.render(
   <React.StrictMode>
