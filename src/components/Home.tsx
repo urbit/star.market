76d418f870f8e20bfa4c8a1fac6f216b987fed8a
Gavin Atkinson <gvnatknsn@protonmail.com>
2021-10-14 18:05:54 -0400

    timer

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,16 +1,56 @@
+import { useEffect, useState } from 'react'
 import HomeHeader from './Header/HomeHeader';
 import Footer from './Footer/Footer';
 import Logo from './Icons/LightLogo';
 import BubbleLink from './Generics/BubbleLink';
 
+function padZero(str:string) {
+    if (str.length < 2) {
+        return `0${str}`
+    }
+    return str
+}
+
 export default function Home() {
+    const calculateTimeLeft = () => {
+        let year = new Date().getFullYear();
+        const difference = +new Date(`${year}-10-30`) - +new Date();
+        let timeLeft = {
+            days: '00',
+            hours: '00',
+            minutes: '00',
+            seconds: '00'
+        };
+    
+        if (difference > 0) {
+          timeLeft = {
+            days: padZero(`${Math.floor(difference / (1000 * 60 * 60 * 24))}`),
+            hours: padZero(`${Math.floor((difference / (1000 * 60 * 60)) % 24)}`),
+            minutes: padZero(`${Math.floor((difference / 1000 / 60) % 60)}`),
+            seconds: padZero(`${Math.floor((difference / 1000) % 60)}`),
+          };
+        }
+    
+        return timeLeft;
+      };
+
+      const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+      useEffect(() => {
+        setTimeout(() => {
+          setTimeLeft(calculateTimeLeft());
+        }, 1000);
+      });
+
+      console.log(timeLeft)
+
     return (
         <div className="layout-container">
             <HomeHeader />
             <section className="layout">
                 <div className="hero-rect" style={{backgroundImage: "url('/assets/hero.jpg')"}}>
                     <Logo className="logo" />
-                    <div className="inner-text measure-narrow">
+                    <div className="inner-text measure-narrow timer">
                         <p>Star Market is the easiest 
 way to obtain an Urbit star.</p>
 
@@ -18,13 +58,17 @@ way to obtain an Urbit star.</p>
                         other provider services on 
                         the Urbit Network.</p>
 
+                        <p className="timer bg-yellow text-black table">
+                            Launching in <b>{timeLeft.days}</b><b>:</b><b>{timeLeft.hours}</b><b>:</b><b>{timeLeft.minutes}</b><b>:</b><b>{timeLeft.seconds}</b>
+                        </p>
+
                     </div>
                 </div>
             </section>
 
             <section className="layout">
                 <div className="flex justify-between">
-                    <div className="measure-narrow mr-1em">
+                    <div className="measure mr-1em">
                         <h3 className="gray-title">Why Urbit ID?</h3>
                         <h2>
                             A key to the world’s only personal computer
@@ -47,7 +91,7 @@ way to obtain an Urbit star.</p>
 
             <section className="layout">
                 <div className="flex justify-between">
-                    <div className="measure-narrow mr-1em">
+                    <div className="measure mr-1em">
                         <h3 className="gray-title">Why Urbit ID?</h3>
                         <h2>
                             A key to the world’s only personal computer
@@ -99,7 +143,7 @@ way to obtain an Urbit star.</p>
 
             <section className="layout">
                 <div className="flex justify-between">
-                    <div className="measure-narrow mr-1em">
+                    <div className="measure mr-1em">
                         <h3 className="gray-title">Urbit IDs for Star Operators</h3>
                         <h2>
                             Stars are businesses, supernodes, providers etc
@@ -127,8 +171,10 @@ way to obtain an Urbit star.</p>
                         Trade WSTR on a variety of distributed exchanges
                     </h2>
 
+                    <p>Coming Soon</p>
+
                     <ul>
-                        <BubbleLink
+                        {/* <BubbleLink
                             href="https://app.uniswap.org/"
                             title="Uniswap V2"
                             caption="A decentralized finance protocol"
@@ -136,7 +182,7 @@ way to obtain an Urbit star.</p>
                             <img 
                             alt="uniswap logo"
                             className="bubble" src="/assets/uniswap-logo.png" />
-                        </BubbleLink>
+                        </BubbleLink> */}
 
                         {/* <BubbleLink
                             href="https://sushi.com/"
