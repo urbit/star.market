fc56daa64b21cf98a530c6c4db5814ef87da9937
Shawn Tobin <shawn.david.tobin@gmail.com>
2023-11-15 12:12:48 +0800

    removed wstr
    - kept 'WSTR App' link in footer, which will still link to the wstr converter app.

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,51 +1,48 @@
-import { Link } from 'react-router-dom';
-import Logo from '../Icons/Logo'
+import { Link } from "react-router-dom";
+import Logo from "../Icons/Logo";
 
-import './Footer.scss'
+import "./Footer.scss";
 
 type FooterProps = {
-    className: string,
-    style: any,
-}
+  className: string;
+  style: any;
+};
 
 export default function Footer({ className, style }: FooterProps) {
-    return (
-        <footer className={`footer ${className}`} style={{ ...style }}>
-            <div className="innerCol">
-                <Link to="/">
-                    <div className="toolbar-link">
-                        <Logo />
-                        <p className=" ml-0.5em">
-                            Star Market
-                        </p>
-                    </div>
-                </Link>
-                <nav>
-                    <Link to="/app">
-                        <div className="toolbar-link">
-                            App
-                        </div>
-                    </Link>
-                    <Link to="/disclaimer">
-                        <div className="toolbar-link">
-                            Disclaimer
-                        </div>
-                    </Link>
-                    <Link to="/tos">
-                        <div className="toolbar-link">
-                            Terms of Service
-                        </div>
-                    </Link>
-                    <a href="https://github.com/urbit/starketplace/issues/new?assignees=&labels=&template=bug_report.md&title=" target="_blank" rel="noreferrer" className="toolbar-link">
-                        Bug Reports
-                    </a>
-                </nav>
-            </div>
-        </footer>
-    )
+  return (
+    <footer className={`footer ${className}`} style={{ ...style }}>
+      <div className="innerCol">
+        <Link to="/">
+          <div className="toolbar-link">
+            <Logo />
+            <p className=" ml-0.5em">Star Market</p>
+          </div>
+        </Link>
+        <nav>
+          <Link to="/app">
+            <div className="toolbar-link">WSTR App</div>
+          </Link>
+          <Link to="/disclaimer">
+            <div className="toolbar-link">Disclaimer</div>
+          </Link>
+          <Link to="/tos">
+            <div className="toolbar-link">Terms of Service</div>
+          </Link>
+          <a
+            href="https://github.com/urbit/starketplace/issues/new?assignees=&labels=&template=bug_report.md&title="
+            target="_blank"
+            rel="noreferrer"
+            className="toolbar-link"
+          >
+            Bug Reports
+          </a>
+        </nav>
+      </div>
+    </footer>
+  );
 }
 
 Footer.defaultProps = {
-    className: "",
-    style: {},
-}
+  className: "",
+  style: {},
+};
